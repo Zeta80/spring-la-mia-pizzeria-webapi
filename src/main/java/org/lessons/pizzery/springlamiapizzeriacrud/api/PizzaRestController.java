f23841529056887530cb5695125dd0ad2e97f55f
Mirko Robbi <mirko.robbi@gmail.com>
2023-04-11 15:26:32 +0200

    ADD api-mapping

---
+++ src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -0,0 +1,94 @@
+package org.lessons.pizzery.springlamiapizzeriacrud.api;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+
+import org.lessons.pizzery.springlamiapizzeriacrud.exceptions.PizzaNotFoundException;
+import org.lessons.pizzery.springlamiapizzeriacrud.model.Offer;
+import org.lessons.pizzery.springlamiapizzeriacrud.model.Pizza;
+import org.lessons.pizzery.springlamiapizzeriacrud.service.PizzaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api/v1/pizzas")
+public class PizzaRestController {
+
+    @Autowired
+    private PizzaService pizzaService;
+
+    // lista di tutti i books
+    @GetMapping
+    public List<Pizza> list(@RequestParam(name = "q") Optional<String> search) {
+        if (search.isPresent()) {
+            return pizzaService.getFilteredPizzas(search.get());
+        }
+        return pizzaService.getAllPizzas();
+    }
+
+    @GetMapping("/{id}")
+    public Pizza getById(@PathVariable Integer id) {
+        try {
+            return pizzaService.getById(id);
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    // create book
+    @PostMapping
+    public Pizza create(@Valid @RequestBody Pizza pizza) {
+
+        return pizzaService.createPizza(pizza);
+    }
+
+    // update book
+    @PutMapping("/{id}")
+    public Pizza update(@PathVariable Integer id, @Valid @RequestBody Pizza pizza) {
+        try {
+            return pizzaService.updatePizza(pizza, id);
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        try {
+            boolean success = pizzaService.deleteById(id);
+            if (!success) {
+                throw new ResponseStatusException(HttpStatus.CONFLICT,
+                        "Unable to delete pizza because it has offerte");
+            }
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+
+    @GetMapping("/{id}/offers")
+    public List<Offer> getBookBorrowings(@PathVariable("id") Integer pizzaId) {
+        Pizza pizza = null;
+        try {
+            pizza = pizzaService.getById(pizzaId);
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return pizza.getOffers();
+    }
+}
--- src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/model/Pizza.java
+++ src/main/java/org/lessons/pizzery/springlamiapizzeriacrud/model/Pizza.java
@@ -1,5 +1,6 @@
 package org.lessons.pizzery.springlamiapizzeriacrud.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotEmpty;
@@ -30,6 +31,7 @@ public class Pizza {
     private Double prezzo;
     private LocalDateTime createdAt;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "pizza")
     private List<Offer> offers;
 
